7d7b2d409060ca6d6dce7930056ce5677a6890ec
pandaspartan2 <90705933+pandaspartan2@users.noreply.github.com>
2021-09-21 09:16:02 -0500

    added Vstack

--- wheelofanime/wheelofanime/ContentView.swift
+++ wheelofanime/wheelofanime/ContentView.swift
@@ -12,8 +12,12 @@ struct ContentView: View {
     @State var selected: Int = 0
     
     var body: some View {
-        Text("Wheel Of Anime!")
-            .padding()
+        
+       
+        VStack {
+            Text("Wheel Of Anime!")
+                .padding()
+        
         
         TabView(selection: $selected) {
             
@@ -26,12 +30,19 @@ struct ContentView: View {
                             .frame(width: 200.0, height: 200.0)
                             .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
                     }
+                    .frame(width: 200.0, height: 200.0)
+                    .cornerRadius(15)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(15)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: -5, y: 5)
                 }
             }
         }
+        .tabViewStyle(PageTabViewStyle (indexDisplayMode: .never))
     }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
